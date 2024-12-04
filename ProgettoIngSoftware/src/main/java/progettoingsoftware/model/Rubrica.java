2f5ceb7fdd81d9a427c421bec5be35e699c833df
gcucc <gcucc@172.19.133.95>
2024-12-04 09:49:23 +0100

    CREAZIONE Rubrica

--- ProgettoIngSoftware/src/main/java/progettoingsoftware/model/Rubrica.java
+++ ProgettoIngSoftware/src/main/java/progettoingsoftware/model/Rubrica.java
@@ -5,12 +5,43 @@
  */
 package progettoingsoftware.model;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  *
  * @author User
  */
 public class Rubrica{
     
+private Set<Contatto> contatti;
+    
+    public Rubrica(TreeSet<Contatto> contatti){
+        this.contatti = contatti;
+    }
+    
+    public void creaContatto() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
+    public void modificaContatto() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void eliminaContatto() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public TreeSet<Contatto> ricercaContatto() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void esportaRubrica() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public TreeSet<Contatto> importaRubrica() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
 
